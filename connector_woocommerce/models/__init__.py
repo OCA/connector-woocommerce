8e450b17d35e304bfefe6d05c0cba5bbb2d8c577
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2018-10-01 12:24:51 +0530

    [ADD/Mig]Migrated WooCommerce module and added Export functions

---
+++ connector_woocommerce/models/__init__.py
@@ -0,0 +1,12 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import woo_binding
+from . import woo_backend
+from . import customer
+from . import product
+from . import product_category
+from . import queue_job
+from . import sale
+from . import shipping_methods
+from . import shipping_zone
+
---
+++ connector_woocommerce/models/woo_binding/__init__.py
@@ -0,0 +1,6 @@
+# © 2009 Tech-Receptives Solutions Pvt. Ltd.
+# © 2018 Serpent Consulting Services Pvt. Ltd.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# See LICENSE file for full copyright and licensing details.
+
+from . import common
