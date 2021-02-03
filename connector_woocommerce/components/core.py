8f5123d46e781c5ca3075f7e9875e861f12597a0
cubells <vicent@vcubells.net>
2021-02-03 20:34:20 +0100

    [MIG] connector_woocommerce: Migration to 12.0

--- connector_woocommerce/components/core.py
+++ connector_woocommerce/components/core.py
@@ -1,16 +1,15 @@
-# © 2009 Tech-Receptives Solutions Pvt. Ltd.
-# © 2018 Serpent Consulting Services Pvt. Ltd.
+# Copyright 2018 FactorLibre
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.addons.component.core import AbstractComponent
 
 
-class BaseWooConnectorComponent(AbstractComponent):
-    """ Base Woo Connector Component
+class BaseWoocommerceConnectorComponent(AbstractComponent):
+    """ Base Woocommerce Connector Component
 
     All components of this connector should inherit from it.
-    """
 
-    _name = 'base.woo.connector'
-    _inherit = 'base.connector'
-    _collection = 'woo.backend'
+    """
+    _name = "base.woocommerce.connector"
+    _inherit = "base.connector"
+    _collection = "wc.backend"
